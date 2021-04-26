04d00f1339c62e59602b652ccb165b5a01a9f4c1
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-04-26 18:51:16 -0300

    Changing Slug View

--- bigbox/views.py
+++ bigbox/views.py
@@ -35,7 +35,7 @@ class ActivityListView(ListView):
 class ActivityDetailView(DetailView):
 
     model = Activity
-    template_name = 'bigbox/box_detail.html'
+    template_name = 'bigbox/boxes_detail.html'
     
 
 class BoxSlugDetailView(DetailView):
