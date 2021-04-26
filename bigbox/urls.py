a34eae3c30eb2b45d356ec8d3a651ced86ddb8ea
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-04-26 19:40:57 -0300

    Changing Activity View

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('base_url/box/', BoxListView.as_view(), name='box-list-view'),
     path('base_url/box/<int:pk>/', BoxDetailView.as_view(), name='box-detail-view'),
     path('base_url/box/<int:pk>/activity/', ActivityListView.as_view(), name='activity-list-view'),
-    path('base_url/box/<int:pk>/activity/<int:pk>', ActivityDetailView.as_view(), name='activity-detail-view'),
-    path('base_url/box/<slug>', BoxSlugDetailView.as_view(), name= 'slug-view')
+    path('base_url/box/<int:pk>/activity/<int:pk>/', ActivityDetailView.as_view(), name='activity-detail-view'),
+    path('base_url/box/<slug>/', BoxSlugDetailView.as_view(), name= 'slug-view')
 ]
