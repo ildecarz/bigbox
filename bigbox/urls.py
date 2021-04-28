9c4b0076afb6b4edf13ad2caf8e852f4b971daac
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-04-28 07:53:05 -0300

    Changing template

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('base_url/box/', BoxListView.as_view(), name='box-list-view'),
     path('base_url/box/<int:pk>/', BoxDetailView.as_view(), name='box-detail-view'),
     path('base_url/box/<int:pk>/activity/', ActivityListView.as_view(), name='activity-list-view'),
-    path('base_url/box/<int:pk_1 >/activity/<int:pk>/', BoxActivityDetailView.as_view(), name='box-activity-detail-view'),
+    path('base_url/box/<int:pk_1>/activity/<int:pk>/', BoxActivityDetailView.as_view(), name='box-activity-detail-view'),
     path('base_url/box/<slug>/', BoxSlugDetailView.as_view(), name= 'slug-view')
 ]
