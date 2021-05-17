1cfca98f302f94e233a12c360fe9858dfe77999e
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-17 10:30:06 -0300

    adding toolbar

--- bigbox_test/settings.py
+++ bigbox_test/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local apps
     'bigbox',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'bigbox_test.urls'
@@ -130,3 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
  
 django_heroku.settings(locals())
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
