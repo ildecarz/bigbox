0c2e209043d51848dbbb24475bda5c2ab86ba8ce
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-17 12:20:41 -0300

    adding toolbar

--- bigbox_test/settings.py
+++ bigbox_test/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 INTERNAL_IPS = ['127.0.0.1']
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda x: True
+}
+
--- bigbox_test/urls.py
+++ bigbox_test/urls.py
@@ -17,10 +17,18 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('bigbox.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
