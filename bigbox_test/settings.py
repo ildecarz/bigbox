3084b63708713e5987819aa2482061090e291b3e
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-17 14:11:16 -0300

    new app

--- bigbox_test/settings.py
+++ bigbox_test/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django_extensions',
     # Local apps
     'bigbox',
+    'polls',
 ]
 
 MIDDLEWARE = [
--- bigbox_test/urls.py
+++ bigbox_test/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
@@ -24,7 +25,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('bigbox.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
+    # path('__debug__/', include(debug_toolbar.urls)),
+    path('polls/', include('polls.urls')),
 ]
 
 if settings.DEBUG:
