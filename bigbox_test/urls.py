6b6f381a14bec9acf830ab5ac0bc1fd961da4b36
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-20 11:55:47 -0300

    new app blog

--- bigbox_test/urls.py
+++ bigbox_test/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     # path('__debug__/', include(debug_toolbar.urls)),
     path('polls/', include('polls.urls')),
     path('', include('snippets.urls')),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),    
 ]
 
