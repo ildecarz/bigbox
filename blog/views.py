cf2c6472bf9b37d93b53a646593030a4500dcec1
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-21 13:44:44 -0300

    new home view

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 
+
+def home(request):
+    return render(request, 'blog/home.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
