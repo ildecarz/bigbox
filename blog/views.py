52f6015d032cbaabe35d624c0175e61d937de758
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-21 14:12:28 -0300

    settings

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,6 @@ from .models import Post
 from .forms import PostForm
 
 
-def home(request):
-    return render(request, 'blog/home.html')
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
