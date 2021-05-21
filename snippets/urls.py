79fed76d5d81d1c4893b4740a37ea268e7fd659f
Ildemaro Carrasco <ildemarocarrasco@gmail.com>
2021-05-21 16:27:52 -0300

    Api views

--- snippets/urls.py
+++ snippets/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
     path('', include(router.urls)),
 ]
 
-# urlpatterns = format_suffix_patterns([
-#    path('', api_root),
-#    path('snippets/', snippet_list, name='snippet-list'),
-#    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-#    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-#    path('users/', user_list, name='user-list'),
-#    path('users/<int:pk>/', user_detail, name='user-detail')
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
+]
